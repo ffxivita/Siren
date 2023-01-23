b941e26ba509ca4cb87d49d487f8eafb47c19def
Luca <luca@ahd-creative.agency>
2023-01-23 20:36:27 +0000

    cache initialization

---
+++ Siren/UserInterface/Images.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Numerics;
+using ImGuiNET;
+using ImGuiScene;
+using Siren.Caching;
+
+namespace Siren.UserInterface
+{
+    public class Images
+    {
+        
+    }
+}
