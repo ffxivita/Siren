d26c1a085a7d7962786eba83a24a97e7ae140504
Luca <luca@ahd-creative.agency>
2023-01-23 16:52:12 +0000

    Rebase

--- Siren/Extensions/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-namespace Siren.Extensions
-{
-    public static class StringExtensions
-    {
-        public static string TrimWhitepace(this string str) => new(str.Where(c => !char.IsWhiteSpace(c)).ToArray());
-    }
-}
