15ea094e6306698db9dccca7e0bb03c34f1c60a4
Luca <luca@ahd-creative.agency>
2023-01-24 06:30:45 +0000

    UserInterfaces/Tooltips

---
+++ Siren/UserInterface/Tooltips.cs
@@ -0,0 +1,22 @@
+using ImGuiNET;
+
+namespace Siren.UserInterface
+{
+    /// <summary>
+    ///     A collection of UI elements for ImGui.
+    /// </summary>
+    public static partial class SiUI
+    {
+        /// <summary>
+        ///     Adds a tooltip to the last item when hovered.
+        /// </summary>
+        /// <param name="tooltip">The tooltip text.</param>
+        public static void TooltipLast(string tooltip)
+        {
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip(tooltip);
+            }
+        }
+    }
+}
