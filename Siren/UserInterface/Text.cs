e2567cf94295df4c5b8c77efb5016203a3fcf2ae
Luca <luca@ahd-creative.agency>
2023-01-24 06:29:28 +0000

    UserInterfaces/Text

---
+++ Siren/UserInterface/Text.cs
@@ -0,0 +1,20 @@
+using FFXIVClientStructs.FFXIV.Common.Math;
+using ImGuiNET;
+
+namespace Siren.UserInterface
+{
+    public static partial class SiUI
+    {
+        /// <summary>
+        ///     Draws coloured text that wraps.
+        /// </summary>
+        /// <param name="colour"></param>
+        /// <param name="text"></param>
+        public static void TextWrappedColoured(Vector4 colour, string text)
+        {
+            ImGui.PushStyleColor(ImGuiCol.Text, colour);
+            ImGui.TextWrapped(text);
+            ImGui.PopStyleColor();
+        }
+    }
+}
